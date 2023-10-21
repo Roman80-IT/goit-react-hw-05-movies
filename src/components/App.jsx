bd47ae027d2a3eed66186012f081a75fe0e8b1b8
Roman <jrj80mail@gmail.com>
2023-10-21 22:44:57 +0300

    start init

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  console.log(Boolean(0) ? 'JavaScript' : 'Java');
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
