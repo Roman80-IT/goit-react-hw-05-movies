aa0ca93cd1e206d5ccb289ee2eb4efb8ae85a349
Roman <jrj80mail@gmail.com>
2023-10-25 16:58:17 +0300

    +Header +General.styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 
+import Layout from 'components/Layout/Layout';
 import Cast from 'pages/Cast';
 import Home from 'pages/Home';
 import MovieDetails from 'pages/MovieDetails';
@@ -9,12 +10,13 @@ import Reviews from 'pages/Reviews';
 export const App = () => {
   return (
     <Routes>
-      {/* <Route path="/" component={Home} /> */}
-      <Route path="/" element={<Home />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/movies/:movieId" element={<MovieDetails />} />
-      <Route path="/movies/:movieId/cast" element={<Cast />} />
-      <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetails />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+      </Route>
     </Routes>
   );
 };
